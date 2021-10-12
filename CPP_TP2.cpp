dae024f1215c738514191677c4f782b2ecd66059
Raph6792 <91563476+Raph6792@users.noreply.github.com>
2021-10-12 23:58:01 +0200

    TP2 - Rectangle

---
+++ CPP_TP2.cpp
@@ -0,0 +1,14 @@
+#include "rectangle.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	rectangle r1(2, 10), r2(8, 15);
+	r1.AffRectangle();
+	cout << endl;
+	r2.AffRectangle();
+
+}
---
+++ StructPoint.h
@@ -0,0 +1,11 @@
+#pragma once
+
+    // I La structure Point //
+struct Point 
+{
+      float x;
+      float y;
+      int a, b, c;
+};
+
+
